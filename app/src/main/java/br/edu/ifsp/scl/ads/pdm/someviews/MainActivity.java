507f9f26121f6ffecbb906d58fa9fdf603a1b94f
northonh <nobile.pedro@gmail.com>
2021-04-06 17:53:59 -0300

    Binding views e seleção de item de Spinner.

--- app/src/main/java/br/edu/ifsp/scl/ads/pdm/someviews/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/someviews/MainActivity.java
@@ -4,16 +4,69 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    // Objetos de binding com as Views
+    private EditText nomeEt;
+    private EditText sobrenomeEt;
+    private EditText emailEt;
+    private Spinner estadoCivilSp;
+    private LinearLayout conjugeLl;
+    private EditText nomeConjugeEt;
+    private EditText sobrenomeConjugeEt;
+    private RadioGroup sexoRg;
+    private RadioButton masculinoRb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Ligando (binding) objetos com as Views
+        bindViews();
+
+        // Listener de item selecionado
+        estadoCivilSp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (((TextView) view).getText().equals("Casado")){
+                    conjugeLl.setVisibility(View.VISIBLE);
+                }
+                else {
+                    conjugeLl.setVisibility(View.GONE);
+                    nomeConjugeEt.setText("");
+                    sobrenomeConjugeEt.setText("");
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
     public void onClickButton(View view){
 
     }
+
+    private void bindViews() {
+        nomeEt = findViewById(R.id.nomeEt);
+        sobrenomeEt = findViewById(R.id.sobrenomeEt);
+        emailEt = findViewById(R.id.emailEt);
+        estadoCivilSp = findViewById(R.id.estadoCivilSp);
+        conjugeLl = findViewById(R.id.conjugeLl);
+        nomeConjugeEt = findViewById(R.id.nomeConjugeEt);
+        sobrenomeConjugeEt = findViewById(R.id.sobrenomeConjugeTv);
+        sexoRg = findViewById(R.id.sexoRg);
+        masculinoRb = findViewById(R.id.masculinoRb);
+    }
+
 }
